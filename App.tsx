a49d1214d06e0339b3d29bbfeb3f70a4bffa9825
Tan Chong How <chong.how97@live.com>
2020-12-24 00:21:45 +0800

    Learn community-hooks

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Dimensions,
+	SafeAreaView,
+	Button,
+	Alert,
+	StatusBar,
+	View,
+	Platform,
+} from 'react-native';
+
+import {
+	useDimensions,
+	useDeviceOrientation,
+} from '@react-native-community/hooks';
 
 export default function App() {
+	const { landscape } = useDeviceOrientation();
 	return (
-		<View style={styles.container}>
-			<Text>Hello world</Text>
-			<StatusBar style="auto" />
-		</View>
+		<SafeAreaView style={[containerStyle, styles.container]}>
+			<View
+				style={{
+					backgroundColor: 'dodgerblue',
+					width: '100%',
+					height: landscape ? '100%' : '30%',
+				}}
+			></View>
+		</SafeAreaView>
 	);
 }
+
+const containerStyle = { backgroundColor: 'orange' };
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
 	},
 });
